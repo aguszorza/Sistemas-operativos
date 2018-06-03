17356dbae9b497379314a713d869e70acc833905
feldmatias <feldmatias@gmail.com>
2018-06-03 19:46:53 -0300

    arreglo memoria

--- 1-creacion de stacks/kern2.c
+++ 1-creacion de stacks/kern2.c
@@ -6,21 +6,21 @@ void print_memory(const multiboot_info_t* mbi){
 	char mem[256] = "Physical memory: ";
 	char tmp[64] = "";
 
-	if (fmt_int(mbi->mmap_length, tmp, sizeof tmp)) {
+	int size = (mbi->mem_upper - mbi->mem_lower) >> 10;
+	if (fmt_int(size, tmp, sizeof tmp)) {
 	    strlcat(mem, tmp, sizeof mem);
 	    strlcat(mem, " MiB total", sizeof mem);
 	}
 
 	char tmp2[64] = "";
-	if (fmt_int(mbi->mmap_addr, tmp2, sizeof tmp2)) {
+	if (fmt_int(mbi->mem_lower, tmp2, sizeof tmp2)) {
 		strlcat(mem, " (", sizeof mem);
 	    strlcat(mem, tmp2, sizeof mem);
 	    strlcat(mem, " KiB base", sizeof mem);
 	}
 
 	char tmp3[64] = "";
-	int bound = mbi->mmap_addr + (mbi->mmap_length << 10);
-	if (fmt_int(bound, tmp3, sizeof tmp3)) {
+	if (fmt_int(mbi->mem_upper, tmp3, sizeof tmp3)) {
 		strlcat(mem, " , ", sizeof mem);
 	    strlcat(mem, tmp3, sizeof mem);
 	    strlcat(mem, " KiB extended) ", sizeof mem);
@@ -44,7 +44,3 @@ void kmain(const multiboot_info_t* mbi) {
     }
 
 }
-
-
-
-/////////////////////preguntar no funciona cambiar tamanio memoria con  -m 256
