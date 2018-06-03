7fcd69ee2db0b52fd164fb000fc66b585675d5c9
feldmatias <feldmatias@gmail.com>
2018-06-03 14:48:20 -0300

    cmdline

--- 1-creacion de stacks/kern2.c
+++ 1-creacion de stacks/kern2.c
@@ -1,11 +1,18 @@
 #include "decls.h"
 #include "multiboot.h"
+#include "lib/string.h"
 
+#define BUF_SIZE 256
 
 void kmain(const multiboot_info_t* mbi) {
-	if (mbi == 0) {
-		vga_write("NULL", 8, 0x70);
-	} else {
-		vga_write("kern2 loading.............", 8, 0x70);
-	}
+	vga_write("kern2 loading.............", 8, 0x70);
+
+	if (mbi->flags) {
+        char buf[BUF_SIZE] = "cmdline: ";
+        char *cmdline = (void *) mbi->cmdline;
+        // Aquí usar strlcat() para concatenar cmdline a buf.
+        strlcat (buf + strlen(buf), cmdline, BUF_SIZE - strlen(buf));
+        vga_write(buf, 9, 0x07);
+    }
+
 }
