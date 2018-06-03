71e0076f9e6667f0a6f5e8694f6f422a75b10f3f
feldmatias <feldmatias@gmail.com>
2018-06-03 15:43:20 -0300

    meminfo --- falta preguntar algo

--- 1-creacion de stacks/write.c
+++ 1-creacion de stacks/write.c
@@ -16,3 +16,29 @@ void vga_write(const char *s, int8_t linea, uint8_t color) {
 		*buff++ = color;
 	}
 }
+
+
+bool fmt_int(uint64_t val, char *s, size_t bufsize){
+	uint64_t digits = 0;
+	uint64_t aux = val;
+	while (aux > 0){
+		digits++;
+		aux /= 10;
+	}
+
+	if (digits >= bufsize){  //>= para agregar el \0
+		return false;
+	}
+
+	for (int i = digits - 1; i >= 0; i--){
+		s[i] = val % 10 + '0';
+		val /= 10;
+	}
+	s[bufsize - 1] = '\0';
+	return true;
+}
+
+
+
+
+//12345 < 10000
