85768d2d1a03636ffba903892814e0352f766bfd
feldmatias <feldmatias@gmail.com>
2018-06-15 14:49:33 -0300

    cambios

---
+++ kern2 final/kern2.c
@@ -0,0 +1,39 @@
+#include "decls.h"
+
+#define USTACK_SIZE 4096
+
+void kmain(const multiboot_info_t *mbi) {
+    vga_write("kern2 loading.............", 8, 0x70);
+
+    if (mbi && mbi->flags) {
+        char buf[256] = "cmdline: ";
+        char *cmdline = (void *) mbi->cmdline;
+
+        strlcat (buf, cmdline, sizeof(buf));
+        vga_write(buf, 9, 0x07);
+
+        print_mbinfo(mbi);
+    }
+
+    two_stacks();
+    two_stacks_c();
+
+    contador_run();
+
+
+    idt_init();
+    irq_init();
+    asm("int3");
+
+
+    int8_t linea;
+    uint8_t color;
+
+    asm("div %4"
+        : "=a"(linea), "=c"(color)
+        : "0"(18), "1"(0xE0), "b"(0), "d"(0));
+
+    vga_write2("Funciona vga_write2?", linea, color);
+}
+
+
